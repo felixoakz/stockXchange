8273e546cda7fc7aaba07a43a19cf7c710121b06
felixoakz <felix@oak.org>
2023-01-28 19:23:41 -0300

    starting project one

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
@@ -6,7 +7,6 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import apology, login_required, lookup, usd
-import datetime
 
 # Configure application
 app = Flask(__name__)
