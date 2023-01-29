c24055c8b4f17143815ccb618d1bae76034f9d35
felixoakz <felix@oak.org>
2023-01-29 20:33:23 -0300

    deployed on render

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import datetime
+import sqlite
 
-from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
